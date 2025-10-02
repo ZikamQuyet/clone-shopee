a3d3971d98e64de1dcb0e3ebd7b2d4aad797d161
nmq <nmq120898@gmail.com>
2025-10-02 23:59:09 +0700

    update footer&header

---
+++ src/components/Footer/Footer.tsx
@@ -0,0 +1,36 @@
+import React from 'react'
+
+const Footer = () => {
+  return (
+    <footer className='py-16 bg-neutral-100'>
+      <div className='px-4 mx-auto max-w-7xl'>
+        <div className='grid grid-cols-1 gap-4 lg:grid-cols-2'>
+          <div className='lg:col-spa-1'>© 2025 Shopee. Tất cả các quyền được bảo lưu.</div>
+          <div className='lg:col-span2'>
+            <div>
+              Quốc gia & Khu vực: Singapore Indonesia Thái Lan Malaysia Việt Nam Philippines Brazil México Colombia
+              Chile Đài Loan
+            </div>
+          </div>
+        </div>
+        <div className='mt-10 text-sm text-center'>
+          <div>Công ty TNHH Shopee</div>
+          <div className='mt-6'>
+            Địa chỉ: Tầng 4-5-6, Tòa nhà Capital Place, số 29 đường Liễu Giai, Phường Ngọc Hà, Thành phố Hà Nội, Việt
+            Nam
+          </div>
+          <div className='mt-2'>
+            Chăm sóc khách hàng: Gọi tổng đài Shopee (miễn phí) hoặc Trò chuyện với Shopee ngay trên Trung tâm trợ giúp
+          </div>
+          <div className='mt-2'>Chịu Trách Nhiệm Quản Lý Nội Dung: Nguyễn Bùi Anh Tuấn</div>
+          <div className='mt-2'>
+            Mã số doanh nghiệp: 0106773786 do Sở Kế hoạch và Đầu tư TP Hà Nội cấp lần đầu ngày 10/02/2015
+          </div>
+          <div className='mt-2'>© 2015 - Bản quyền thuộc về Công ty TNHH Shopee</div>
+        </div>
+      </div>
+    </footer>
+  )
+}
+
+export default Footer
--- src/layouts/RegisterLayout/RegisterLayout.tsx
+++ src/layouts/RegisterLayout/RegisterLayout.tsx
@@ -1,10 +1,18 @@
 import React from 'react'
+import RegisterHeader from '../../components/RegisterHeader'
+import Footer from '../../components/Footer'
 
 interface IProps {
   children?: React.ReactNode
 }
 const RegisterLayout = ({ children }: IProps) => {
-  return <>{children}</>
+  return (
+    <>
+      <RegisterHeader />
+      {children}
+      <Footer />
+    </>
+  )
 }
 
 export default RegisterLayout
